db1963f3729f0f60c35505efa84e5a7e8f233bec
safutrasetya <safutrasetya@gmail.com>
2023-11-08 22:48:33 +0700

    add/ update: komponen table

--- agriculture-react/src/components/Table/Table.jsx
+++ agriculture-react/src/components/Table/Table.jsx
@@ -3,36 +3,52 @@ import "./Table.css"
 
 
 export default function Table({headers, children}){
+    // passing props headers adalah array yang isinya string. bentuknya seprti berikut vv: 
+    // headers = ["No.", "Nama pengguna", "Nama yang lainya", "dll"]
 
-    
-
+    // props children itu adalah apapun yang ada di antara tag Table pembuka dan penutup
+    // contoh penggunaan vv : 
+    // <Table headers={headers}>
+    //     <tr>
+    //             <td>1</td>
+    //             <td>Nama orang disini</td>
+    //             <td>Nomornya euy</td>
+    //             <td>1</td>
+    //             <td>Nama orang disini</td>
+    //             <td>Nomornya euy</td>
+    //         </tr>
+    //         <tr>
+    //             <td>1</td>
+    //             <td>Nama orang disini</td>
+    //             <td>Nomornya euy</td>
+    //         </tr>
+    //         <tr>
+    //             <td>1</td>
+    //             <td>Nama orang disini</td>
+    //             <td>Nomornya euy</td>
+    //         </tr>
+    // </Table>
     return(
         <>
             <table className="table-container">
-                <thead className="table-head">
-                    <tr className="table-row">
-                        <th className="table-th">No.</th>
-                        <th className="table-th">Name</th>
-                        <th className="table-th">Number</th>
+                <thead className="table-head ">
+                    <tr className="table-row ">
+                        {
+                            headers ?
+                            headers.map((items)=>{
+                                return(
+                                    <th className="table-th fontw600">{items}</th>
+                                )
+                            })
+                            :
+                            <th className="table-th fontw600">No Headers Found</th>
+                        }
                     </tr>
                 </thead>
                 <tbody className="table-body">
-                    <tr>
-                        <td>1</td>
-                        <td>Nama orang disini</td>
-                        <td>Nomornya euy</td>
+                    <tr className="table-head-bodygap">
                     </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Nama orang disini</td>
-                        <td>Nomornya euy</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Nama orang disini</td>
-                        <td>Nomornya euy</td>
-                    </tr>
-                
+                    {children}
                 </tbody>
             
             </table>
--- agriculture-react/src/pages/TestPage/TestPage.jsx
+++ agriculture-react/src/pages/TestPage/TestPage.jsx
@@ -2,11 +2,29 @@ import Table from "../../components/Table/Table";
 
 export default function TestPage(){
 
+    const headers= ["Ngetest ioni satu", "kedua", "Ketiga", "keempat"]
     return(
         <>
         <div style={ {padding:"30px", backgroundColor: "grey"}}>
-            <Table>
-                
+            <Table headers={headers}>
+                <tr>
+                        <td>1</td>
+                        <td>Nama orang disini</td>
+                        <td>Nomornya euy</td>
+                        <td>1</td>
+                        <td>Nama orang disini</td>
+                        <td>Nomornya euy</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Nama orang disini</td>
+                        <td>Nomornya euy</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Nama orang disini</td>
+                        <td>Nomornya euy</td>
+                    </tr>
             </Table>
         </div>
         
