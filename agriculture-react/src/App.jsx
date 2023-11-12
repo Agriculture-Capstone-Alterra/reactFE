830f37175e080aee9206aac447d6a0dff8ea3102
yunifhakim13 <yunifhakim@gmail.com>
2023-11-13 04:28:33 +0700

    add topbar

--- agriculture-react/src/App.jsx
+++ agriculture-react/src/App.jsx
@@ -1,23 +1,23 @@
-import { useState } from 'react'
-import {BrowserRouter, Routes, Route, Link, Outlet} from 'react-router-dom';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 
-import "./essentials/essentialcss.css"
-import TestPage from './pages/TestPage/TestPAge';
+import "./essentials/essentialcss.css";
+import TestPage from "./pages/TestPage/TestPAge";
+import Topbar from "./components/Topbar/Topbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<TestPage/>} />
+          <Route path="/" element={<TestPage />} />
+          <Route path="/topbar" element={<Topbar />} />
         </Routes>
       </BrowserRouter>
-
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
