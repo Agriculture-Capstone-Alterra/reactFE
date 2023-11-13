42452ba1ea9996039c5da3ef8e89b0949aba2f52
safutrasetya <safutrasetya@gmail.com>
2023-11-14 00:55:36 +0700

    update layout finished. BEGIN SLICING

--- agriculture-react/src/layout/Layout.jsx
+++ agriculture-react/src/layout/Layout.jsx
@@ -1,21 +1,45 @@
+import { Link } from "react-router-dom";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Topbar from "../components/Topbar/Topbar";
 import "./Layout.css"
 
 
 export default function Layout({children , pagetitle, breadcrumbs}){
-    return (
 
+    const crumbsize = breadcrumbs.length
+    return (
         <>
-        
         <div className="d-flex">
             <Sidebar/>
             <div className="d-flex flex-column layout-rightpart">
                 <Topbar pagetitle={pagetitle} />
                 <div className="layout-breadcrumbsdiv">
                     <p className="fonts12 text-removemargin">
-                        <span className="fontw600">{breadcrumbs}</span>
-                        <span className="fontw600 text-secondary">{breadcrumbs}</span>
+                        {
+                            breadcrumbs ?
+                            breadcrumbs.map((items, index)=>{
+                                
+                                if(index == crumbsize-1){
+                                    return (
+                                        <>
+                                            <Link className="layout-crumblink fontw600 now" to={items.crumblink}>
+                                                <span>  {items.crumbname}</span>
+                                            </Link>
+                                        </>
+                                    )
+                                }
+                                return (
+                                    <>
+                                        <Link className="layout-crumblink fontw600 past" to={items.crumblink}>
+                                            <span> {items.crumbname}</span>
+                                        </Link><span className="fontw600 layout-crumblink-divider">/</span>
+                                    </>
+                                )
+                            }):
+                            <></>
+                        }
+
+                        
                     </p>
                 </div>
                 <div className="layout-maincontent">
