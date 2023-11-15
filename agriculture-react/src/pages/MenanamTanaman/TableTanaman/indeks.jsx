e4a04e98cfd770a0fd3c85279438405c97617584
Abdul Hafizh <ahafizh011@gmail.com>
2023-11-15 11:16:33 +0700

    p

---
+++ agriculture-react/src/pages/MenanamTanaman/TableTanaman/indeks.jsx
@@ -0,0 +1,20 @@
+const TableTanaman = () =>{
+
+    const [OpenForm, setOpenForm, setCloseForm] = useState(true);
+
+    return (
+        <div> 
+            <div> 
+            <div>
+                <span className="table-text-title">List Tanaman</span>
+                <button></button>
+            </div>
+            <div>Table</div>
+            <div>next/prev Button</div>
+            </div>
+            <div > filter</div>
+        </div>
+    )
+}
+
+export default TableTanaman;
