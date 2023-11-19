43696a4bbf54b993e6a36d1a51285e52467fb826
IrfanRasyid <41520010081@student.mercubuana.ac.id>
2023-11-19 22:06:49 +0700

    Update Card Component

--- agriculture-react/src/components/Card/Card.jsx
+++ agriculture-react/src/components/Card/Card.jsx
@@ -1,37 +1,30 @@
-
-import React from 'react';
-import MawarPutih from '../../assets/img/mawarputih.jpg';
-import './card.css';
+import React from "react";
+import MawarPutih from "../../assets/img/mawarputih.jpg";
+import Teknologi from "../../assets/Teknologi.svg";
+import jenisTanaman from "../../assets/jenisTanaman.svg";
+import "./card.css";
 
 const Card = () => {
   return (
-    <div className="card" style={{ width: '18rem' }}>
+    <div className="card" style={{ width: "18rem" }}>
       <img src={MawarPutih} className="card-img-top img-style" alt="..." />
       <div className="card-body">
         <h5 className="card-title text-start fw-bold">Mawar Putih</h5>
         <div className="content d-flex justify-content-between">
-            <div className='text-center'>
-            <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/3d57a20b-3aa0-4474-9930-00351f168b37?apiKey=40730d6fa7d54ebf8502a6e2d5d2de4d&"
-        className="img"
-      />
-      <div className="">
-          <div className="fw-bold">Jenis Tanaman</div>
-          <p>Bunga</p>
-        </div>
+          <div className="text-center">
+            <div className="">
+              <div className="fw-bold">Jenis Tanaman</div>
+              <img src={jenisTanaman} alt="teknologi" />
+              <p>Bunga</p>
             </div>
-            <div className='text-center'>
-            <img
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/3d57a20b-3aa0-4474-9930-00351f168b37?apiKey=40730d6fa7d54ebf8502a6e2d5d2de4d&"
-            className="img"
-          />
-        <div className="div-3">
-          <div className="fw-bold">Teknologi</div>
-          <p>Hidroponik</p>
-        </div>
+          </div>
+          <div className="text-center">
+            <div className="div-3">
+              <div className="fw-bold">Teknologi</div>
+              <img src={Teknologi} alt="teknologi" />
+              <p>Hidroponik</p>
             </div>
+          </div>
         </div>
       </div>
     </div>
