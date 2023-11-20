754c2b68cb864c45dbb6877da49d2748d4c4aa1e
AnnisaFitry <91646811+AnnisaFitry@users.noreply.github.com>
2023-11-20 18:34:36 +0700

    update : route

--- agriculture-react/src/App.jsx
+++ agriculture-react/src/App.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 import "./essentials/essentialcss.css";
 import TestPage from "./pages/TestPage/TestPage";
 import Sidebar from "./components/Sidebar/Sidebar";
+import TambahTanaman from "./pages/MenanamTanaman/TambahTanaman";
+import ListTanaman from "./pages/MenanamTanaman/ListTanaman";
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<TestPage />} />
+          <Route path="/menanam-tanaman/tambah-tanaman" element={<TambahTanaman />} />
         </Routes>
     </BrowserRouter>
   );
