27c47c48cad06869a2760909992e96b1d910f241
FadhilAlif <92931140+FadhilAlif@users.noreply.github.com>
2023-11-20 11:40:18 +0700

    add: filter | page List Tanaman

--- agriculture-react/src/pages/RiwayatMenanam/ListTanaman/ListTanaman.jsx
+++ agriculture-react/src/pages/RiwayatMenanam/ListTanaman/ListTanaman.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import { useState } from "react";
 import Card from "../../../components/Card/Card";
 import Layout from "../../../layout/Layout";
 import styles from "./ListTanaman.module.css";
+import Filter from "../../../components/Filter";
 
 const ListTanaman = () => {
+  const cardExample = [
+    {
+      name: "Tanaman 1",
+      date: "12-12-2023",
+    },
+    {
+      name: "Tanaman 2",
+      date: "12-12-2024",
+    },
+    {
+      name: "Tanaman 3",
+      date: "12-12-2025",
+    },
+  ];
+
+  const [cards, setCards] = useState(cardExample);
+
   const breadcrumbsobjectexample = [
     {
-      crumbname: "List Tanaman",
+      crumbname: "Riwayat Menanam",
       crumblink: "/riwayat-menanam",
+    },
+    {
+      crumbname: "List Tanaman",
+      crumblink: "/list-tanaman",
     },
   ];
 
@@ -18,26 +40,27 @@ const ListTanaman = () => {
           <div className={styles.header}>
             <h4 className={styles.title}>List Tanaman </h4>
           </div>
-          <div className={styles.content}>
-            <div className={styles.selectBox}>
-              <select
-                className="form-select form-select-sm"
-                aria-label="Default select example"
-              >
-                <option selected="">Urutkan dalam</option>
-                <option value={1}>Paling lama</option>
-                <option value={2}>Paling baru</option>
-                <option value={3}>A-Z</option>
-                <option value={3}> Z-A</option>
-              </select>
-            </div>
-            <div className={styles.card}>
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
+          <div className={styles.contentContainer}>
+            <select
+              className={styles.selectBox}
+              // onChange={}
+              // value={}
+            >
+              <option value="">Urutkan dalam</option>
+              <option value="1">Paling lama</option>
+              <option value="2">Paling baru</option>
+              <option value="3">A-Z</option>
+              <option value="4">Z-A</option>
+            </select>
+            <div className={styles.cardContainer}>
+              <div className={styles.card}>
+                {cards.map((card, index) => (
+                  <Card key={index} name={card.name} date={card.date} />
+                ))}
+              </div>
+              <div className={styles.filter}>
+                <Filter />
+              </div>
             </div>
           </div>
         </div>
