5cab4ea2cbe23b9658fad3fba651aa1c49a6cba9
IrfanRasyid <41520010081@student.mercubuana.ac.id>
2023-11-21 20:45:00 +0700

    update Card

--- agriculture-react/src/components/Card/Card.jsx
+++ agriculture-react/src/components/Card/Card.jsx
@@ -6,22 +6,22 @@ import "./Card.css";
 
 const Card = () => {
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img src={MawarPutih} className="card-img-top img-style" alt="..." />
+    <div className="card" style={{ width: "274px" }}>
+      <img src={MawarPutih} style={{ height:"184px"}} className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title text-start fw-bold">Mawar Putih</h5>
+        <h5 className="card-title text-start fw-bold" >Mawar Putih</h5>
         <div className="content d-flex justify-content-between">
           <div className="text-center">
             <div className="">
+              <img src={jenisTanaman} style={{ width: "50px", height: "50px" }} alt="teknologi" />
               <div className="fw-bold">Jenis Tanaman</div>
-              <img src={jenisTanaman} alt="teknologi" />
               <p>Bunga</p>
             </div>
           </div>
           <div className="text-center">
             <div className="div-3">
+              <img src={Teknologi} style={{ width: "50px", height: "50px" }} alt="teknologi" />
               <div className="fw-bold">Teknologi</div>
-              <img src={Teknologi} alt="teknologi" />
               <p>Hidroponik</p>
             </div>
           </div>
