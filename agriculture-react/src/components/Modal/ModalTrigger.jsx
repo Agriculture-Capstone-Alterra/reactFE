45ba21ea9917754e78a4a5118b4390990361a4ca
Latorynska <paksiringkang225@gmail.com>
2023-11-28 08:20:26 +0700

    add: delete logic on local state (pengingat tanaman page), update : modal close after click confirm/delete button, add disabled props to modal trigger

--- agriculture-react/src/components/Modal/ModalTrigger.jsx
+++ agriculture-react/src/components/Modal/ModalTrigger.jsx
@@ -53,10 +53,17 @@
     /> 
 */}
 // end : contoh penggunaan lengkap dengan modalnya : 
-const ModalTrigger = ({ className, modalTarget, children, style}) => {
-    console.log(modalTarget);
+const ModalTrigger = ({ className, modalTarget, children, style, disabled, onClick}) => {
+    // console.log(modalTarget);
     return ( 
-        <button className={className} style={style} data-bs-toggle="modal" data-bs-target={`#${modalTarget}`}>
+        <button 
+            className={className} 
+            style={style} 
+            data-bs-toggle="modal" 
+            data-bs-target={`#${modalTarget}`} 
+            onClick={onClick}
+            disabled={disabled} 
+        >
             {children}
         </button>
      );
